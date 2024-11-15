39b45eedfdb9b6bd2e2ed6cf00a549fdb08b8dd2
Fefe81 <162328754+Fefe81@users.noreply.github.com>
2024-11-15 09:18:08 +0100

    first

--- game.py
+++ game.py
@@ -2,6 +2,8 @@ import pygame as pg
 pg.init()
 
 largeur, hauteur = 1200, 800
+ecran = pg.image.load('img/fond.jpeg')
+ecran = pg.transform.scale(ecran, (largeur, hauteur))
 screen = pg.display.set_mode((largeur, hauteur))
 
 continuer = True
@@ -9,4 +11,6 @@ while continuer:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             continuer = False
+    screen.blit(ecran, (0, 0))
+    pg.display.flip()
 pg.quit()
