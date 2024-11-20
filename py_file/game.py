c7fb4f11e8b1d56acb498d8e4a239d5b1ef45a8a
Fefe81 <162328754+Fefe81@users.noreply.github.com>
2024-11-20 09:49:22 +0100

    debut du jeu 2

--- py_file/game.py
+++ py_file/game.py
@@ -1,16 +1,40 @@
-import pygame as pg
-pg.init()
+import pygame
+from player import*
+from player import Player
+pygame.init()
 
 largeur, hauteur = 1200, 800
-ecran = pg.image.load('img/fond.jpeg')
-ecran = pg.transform.scale(ecran, (largeur, hauteur))
-screen = pg.display.set_mode((largeur, hauteur))
+ecran = pygame.image.load('img/fond.jpeg')
+ecran = pygame.transform.scale(ecran, (largeur, hauteur))
+screen = pygame.display.set_mode((largeur, hauteur))
 
+player = Player(0, 320, 0, 0)
+keys_pressed = set()
 continuer = True
 while continuer:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_x: # Ferme le jeu quand on appuie sur la croix ou sur la touche x en mettant la variable continuer à False
             continuer = False
+
+        elif event.type == pygame.QUIT:
+            continuer = False
+
+        if event.type == pygame.KEYDOWN:
+            keys_pressed.add(event.key)
+            if event.key == pygame.K_RIGHT:
+                player.move_right()
+            if event.key == pygame.K_LEFT:
+                player.move_left()              # Gère touts les déplacements du joueurs en fonction de quelle flèche est pressée
+            if event.key == pygame.K_DOWN:
+                player.move_down()
+            if event.key == pygame.K_UP:
+                player.move_up()
+        if event.type == pygame.KEYUP:
+            keys_pressed.discard(event.key) # Retire la touche relâchée de l'ensemble des touches pressées
+            if not keys_pressed.intersection({pygame.K_RIGHT, pygame.K_LEFT, pygame.K_DOWN, pygame.K_UP}):
+                player.stop()
+    player.update()
     screen.blit(ecran, (0, 0))
-    pg.display.flip()
-pg.quit()
+    player.draw(ecran)
+    pygame.display.update()
+pygame.quit()
--- py_file/player.py
+++ py_file/player.py
@@ -6,14 +6,53 @@ from game import*
 
 player_velocity = 5.00
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    player = pygame.image.load('img/perso.png')
+    player = pygame.transform.scale(player, (80, 60))
+
+    def __init__(self, x, y, velocity_x, velocity_y):
         super().__init__()
-        self.image = pygame.image.load('img/perso.png')
-        self.image = pygame.transform.scale(self.image, (40, 60))
-        self.rect = self.image.get_rect
-        self.velocity = player_velocity
-    
+        self.position = [x, y]
+        self.velocity = [velocity_x, velocity_y]
+        self.image = Player.player
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.position
+        self.last_shot_time = 0
+        self.last_spawn_time = 0
+        self.last_spawn1_time = 0
+        self.last_spawn2_time = 0
+        self.mob_velocity = -4.000
+        self.frequence = 550
+
     def update(self):
-        self.rect.x += self.velocity
-        if self.rect.x < 0 or self.rect.y < 0 or self.rect.x > largeur or self.rect.y > hauteur:
-            self.kill()
+        self.position[0] += self.velocity[0]
+        self.position[1] += self.velocity[1]
+
+        if self.position[0] < 0:
+            self.position[0] = 0
+        elif self.position[0] + self.rect.width > 900:
+            self.position[0] = 900 - self.rect.width
+        elif self.position[1] + self.rect.height < 320:
+            self.position[1] = 320 - self.rect.height
+        elif self.position[1] + self.rect.height > 410:
+            self.position[1] = 410 - self.rect.height
+        
+        self.rect.topleft = self.position 
+
+    def draw(self, surface):
+       surface.blit(self.image, self.rect.topleft)
+
+    def move_left(self):
+        self.velocity[0] = -1
+        self.velocity[1] = 0
+    def move_right(self):
+        self.velocity[0] = 1
+        self.velocity[1] = 0
+    def move_up(self):
+        self.velocity[1] = -1
+        self.velocity[0] = 0
+    def move_down(self):
+        self.velocity[1] = 1
+        self.velocity[0] = 0
+    def stop(self):
+        self.velocity[0] = 0
+        self.velocity[1] = 0
