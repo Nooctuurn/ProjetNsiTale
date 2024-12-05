128e962bc998eb4d093a2854eea1e5241f180b47
lolito81 <163427275+lolito81@users.noreply.github.com>
2024-12-05 16:54:14 +0100

    Avancement majeur
    j'ai fait en sorte que le mob puisse bouger de gauche a droite .
    j'ai aussi fait en sorte qu'il est un carrer autour de lui qui représente la porter de ses attaques(pas encore fonctionelle)

--- MAIN.py
+++ MAIN.py
@@ -2,30 +2,53 @@ import pygame
 
 class Bot:
     def __init__(self, nom, pv, porter, degat, image_path):
-        self.nom = nom
-        self.pv = pv
-        self.porter = porter
-        self.degat = degat
-        self.image = pygame.image.load(image_path).convert_alpha()  # Chargement de l'image
+        self.nom = nom # le nom du mob 
+        self.pv = pv # les pv du mob
+        self.porter = porter # la porter des coups du mob
+        self.degat = degat # les degats qu'inflige le mob
+        self.image = pygame.image.load(image_path).convert_alpha()  # charger l'image
+        self.width, self.height = self.image.get_size() # recuperer la taille de l'image
+        self.image = pygame.transform.scale(self.image, (self.width // 2, self.height // 2))  # redimensionnement l'imagge car elle est beaucoup trop grande
+        self.width //= 2 # on met a jour la redimension de l'image pour l'utiliser plus tard (largeur)
+        self.height //= 2 # on met a jour la redimension de l'image pour l'utiliser plus tard (longueur)
 
 pygame.init()
-
 fenetre = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Affichage du Bot")
 
+# initialisation du mob
 bot_de_pierre = Bot("Bot de pierre", 100, 50, 10, 'img/Bot.png')
 
-x_bot, y_bot = 300, 200  # Coordonnées pour dessiner le bot
+# Coordonnées pour dessiner le mob
+x_bot, y_bot = 500, 200  # position de départ
+dx = -1  # Direction initiale
 
 clock = pygame.time.Clock()
-while True:
+running = True
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+            running = False
 
-    fenetre.fill((255, 255, 255))
+    # Mise à jour des coordonnées pour le mouvement
+    x_bot += dx
+
+    if x_bot <= 300:  # si le bot atteint la position 300 il repart vers la droite
+        dx = 1
+    elif x_bot >= 500:  # Si le bot atteint la position 500 il repart vers la gauche
+        dx = -1
+
+    fenetre.fill((255, 255, 255))  # fond blanc
+    
+    # Dessiner un carré rouge autour de l'image pour gerer la porté des attaques
+    rect = pygame.Rect(x_bot, y_bot, bot_de_pierre.width, bot_de_pierre.height)  # creer un rectangle autour de l'image
+    pygame.draw.rect(fenetre, (255, 0, 0), rect, 2)  # Rectangle rouge autour de l'image avec une bordure de 2 pixels
+
+    # Dessiner le bot
     fenetre.blit(bot_de_pierre.image, (x_bot, y_bot))
 
     pygame.display.flip()
     clock.tick(60)
+
+pygame.quit()
