2e0f1129ed3f130e88a68bec9ff4bf383668ff2c
Noctuurn <127550008+Noctuurn@users.noreply.github.com>
2024-12-10 21:40:08 +0100

    Correction du menu
    Correction du problème du jeu qui se fermait et qui se relançait pour changer de mode de jeu.
    BUGS : Le personnage reste bloqué lors du changement du mode de jeu

--- py_file/game.py
+++ py_file/game.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import menu
 from random import randint
 from player import Player
 
@@ -10,48 +11,98 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Smash Banana")
 ecran = pygame.display.set_mode((largeur, hauteur))
 
-background = pygame.image.load('img/fond2.png')
-bg = pygame.transform.scale(background, (largeur, hauteur))
+# Importation des images / icônes
+
+bg_menu = pygame.image.load('img/fond.jpeg')
+bg_menu = pygame.transform.scale(bg_menu,(largeur,hauteur))
+
+bg_jeu = pygame.image.load('img/fond2.png') # Jeu
+bg_jeu = pygame.transform.scale(bg_jeu, (largeur, hauteur)) # Jeu
 
 icon = pygame.image.load('img/iconne.webp')
 pygame.display.set_icon(icon)
 
-player = Player(-60, 500, 0, 0, 10)
+player = ""
 keys_pressed = set()
-continuer = True
 allowed_char = [pygame.K_RIGHT, pygame.K_LEFT]
+gamemode = "Menu"
 
+continuer = True
+print(f"DEBUG : Gamemode -> {gamemode}")
 while continuer:
     clock.tick(60)
-    ecran.blit(bg, (0, 0))
+    if gamemode == "Menu":
+        ecran.blit(bg_menu,(0,0))
+        pygame.display.set_caption("Smash Banana - Menu Principal")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Clic gauche
+                for nom, rect in menu.boutons.items(): # Lance les modes de jeu / paramètres
+                    if rect.collidepoint(event.pos):
+                        if nom == "Solo":
+                            gamemode = "Solo"
+                            print(f"DEBUG : Gamemode -> {gamemode}")
+                            break
+                        
+                        elif nom == "Multijoueur en local":
+                              gamemode = "Multiplayer"
+                              print(f"DEBUG : Gamemode -> {gamemode}")
+                              break
+                        
+                        elif nom == "Paramètres":
+                            gamemode = "Settings"
+                            print(f"DEBUG : Gamemode -> {gamemode}")
+                            break
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            continuer = False
+        for nom, rect in menu.boutons.items():
+            menu.dessiner_bouton(menu.fenetre, rect, nom, menu.GRIS, menu.BLANC)
+        
 
-        elif event.type == pygame.KEYDOWN:
-            if not event.key in allowed_char:
-                break
-            keys_pressed.add(event.key)
-            if event.key == pygame.K_RIGHT:
-                player.move_right()
-            if event.key == pygame.K_LEFT:
-                player.move_left()
-            #if event.key == pygame.K_DOWN:
-                #player.move_down()
-            #if event.key == pygame.K_UP:
-                #player.move_up()
+    if gamemode == "Solo" :
+        ecran.blit(bg_jeu, (0, 0))
+        player = Player(-60, 500, 0, 0, 10)
+        print("DEBUG : BLOQUÉ ICI2")
+        pygame.display.set_caption("Smash Banana - Solo")
+        print("DEBUG : BLOQUÉ ICI1")
 
-        elif event.type == pygame.KEYUP:
-            if not event.key in allowed_char:
-                break
-            keys_pressed.discard(event.key)
-            if not keys_pressed.intersection({pygame.K_RIGHT, pygame.K_LEFT}):
-                    player.stop()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                continuer = False
 
-    # Update and Draw Player
-    player.update()
-    player.draw(ecran)
+            elif event.type == pygame.KEYDOWN:
+                if not event.key in allowed_char:
+                    break
+                keys_pressed.add(event.key)
+                if event.key == pygame.K_RIGHT:
+                    player.move_right()
+                if event.key == pygame.K_LEFT:
+                    player.move_left()
+                #if event.key == pygame.K_DOWN:
+                    #player.move_down()
+                #if event.key == pygame.K_UP:
+                    #player.move_up()
 
-    # Refresh Screen
+            elif event.type == pygame.KEYUP:
+                if not event.key in allowed_char:
+                    break
+                keys_pressed.discard(event.key)
+                if not keys_pressed.intersection({pygame.K_RIGHT, pygame.K_LEFT}):
+                        player.stop()
+
+        player.update()
+        player.draw(ecran)
+
+    if gamemode == "Multiplayer" :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                continuer = False
+
+    if gamemode == "Settings" : 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                continuer = False
+
+
     pygame.display.update()
--- py_file/menu.py
+++ py_file/menu.py
@@ -1,43 +1,28 @@
 import pygame
-import sys
-import subprocess  
-
-# Initialisation de pygame
 pygame.init()
 
-# Dimensions de la fenêtre
 largeur, hauteur = 1200, 600
 fenetre = pygame.display.set_mode((largeur, hauteur))
 pygame.display.set_caption("Smash Banana - Menu Principal")
 
-icon = pygame.image.load('img/iconne.webp')
-pygame.display.set_icon(icon)
-
-# Couleurs
 BLANC = (255, 255, 255)
 GRIS = (169, 169, 169)
 
-# Charger l'image de fond
 fond = pygame.image.load("img/fond.jpeg")
 fond = pygame.transform.scale(fond, (largeur, hauteur))
 
-# Police
 police_titre = pygame.font.Font(None, 80)
 police_boutons = pygame.font.Font(None, 50)
 
-# Texte central
-texte_titre = police_titre.render("Mettre Logo Ici", True, BLANC)
+texte_titre = police_titre.render("Titre", True, BLANC)
 titre_rect = texte_titre.get_rect(center=(largeur // 2, hauteur // 4))
 
-# Définir les boutons
 bouton_largeur, bouton_hauteur = 300, 60
 espacement = 20
 
-# Calculer les positions des boutons
 x_bouton = (largeur - bouton_largeur) // 2
 y_bouton = hauteur // 2
 
-# Créer les rectangles pour les boutons
 boutons = {
     "Solo": pygame.Rect(x_bouton, y_bouton, bouton_largeur, bouton_hauteur),
     "Multijoueur en local": pygame.Rect(x_bouton, y_bouton + bouton_hauteur + espacement, bouton_largeur, bouton_hauteur),
@@ -50,43 +35,3 @@ def dessiner_bouton(surface, rect, texte, couleur_fond, couleur_texte):
     texte_surface = police_boutons.render(texte, True, couleur_texte)
     texte_rect = texte_surface.get_rect(center=rect.center)
     surface.blit(texte_surface, texte_rect)
-
-# Lancer le fichier game.py
-def lancer_jeu(script_name):
-    pygame.quit()  # Ferme la fenêtre pygame
-    subprocess.run(["python", script_name])  # Lance le fichier de jeu
-    sys.exit()  # Quitte le programme après avoir choisi un mode de jeu
-
-# Boucle principale
-def menu():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Clic gauche
-                for nom, rect in boutons.items():
-                    if rect.collidepoint(event.pos):
-                        if nom == "Solo":
-                            lancer_jeu("py_file\game.py")  # Lance les modes de jeu / paramètres
-                        elif nom == "Multijoueur en local":
-                            lancer_jeu("multiplayer_game.py")  
-                        elif nom == "Paramètres":
-                            print("Ouvrir les paramètres")
-                            
-
-        # Affichage du fond
-        fenetre.blit(fond, (0, 0))
-
-        # Afficher le texte central
-        fenetre.blit(texte_titre, titre_rect)
-
-        # Afficher les boutons
-        for nom, rect in boutons.items():
-            dessiner_bouton(fenetre, rect, nom, GRIS, BLANC)
-
-        # Mettre à jour l'affichage
-        pygame.display.flip()
-
-# Lancer le menu
-menu()
