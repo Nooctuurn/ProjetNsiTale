20162f6c0fc5a8dabd908590923d0433e752d4b5
Laurent mazars <127550008+Noctuurn@users.noreply.github.com>
2024-12-20 09:22:34 +0100

    avancées mineures sur le menu

--- py_file/menu.py
+++ py_file/menu.py
@@ -16,6 +16,9 @@ fond = pygame.transform.scale(fond, (largeur, hauteur))
 police_titre = pygame.font.Font(None, 80)
 police_boutons = pygame.font.Font(None, 50)
 
+texture_bouton = pygame.image.load("img/Boutons.png")
+
+
 texte_titre = police_titre.render("Titre", True, BLEU_CLAIR)
 titre_rect = texte_titre.get_rect(center=(largeur // 2, hauteur // 4))
 
@@ -31,15 +34,14 @@ boutons = {
     # Couleur du dessous des boutons : #c2241f
     # Couleur de l'interieur : #21243c
     # Couleur texte : #0fabb7
-    "Solo": pygame.Rect(x_bouton, y_bouton-70, bouton_largeur, bouton_hauteur),
+    "Solo": texture_bouton.blit(fond,x_bouton,y_bouton),
     "Multijoueur en local": pygame.Rect(x_bouton, y_bouton-70+ bouton_hauteur + espacement+ marge, bouton_largeur, bouton_hauteur),
     "Paramètres": pygame.Rect(x_bouton*7, y_bouton , bouton_largeur, bouton_hauteur),
 }
 
 # Fonction pour afficher les boutons et le texte dessus
 def dessiner_bouton(surface, rect, texte, couleur_fond,couleur_marge, couleur_texte):
-    pygame.draw.rect(surface,couleur_marge,rect)
-    pygame.draw.rect(surface, couleur_fond, rect)
+    texture_bouton.blit()
     texte_surface = police_boutons.render(texte, True, couleur_texte)
     texte_rect = texte_surface.get_rect(center=rect.center)
     surface.blit(texte_surface, texte_rect)
